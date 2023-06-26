a892c2eafbab5836f6ac4b7a95f9071a2ba923e0
Sagar Mandal <sagarm7102002@gmail.com>
2023-06-26 23:45:32 +0530

    CORS issue resolved

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 const app = express();
@@ -6,6 +7,7 @@ dotenv.config({ path: './config.env' });
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(require('./routes/homepage'));
 app.use(require('./routes/option'));
 app.get('*', (req, res) => {
